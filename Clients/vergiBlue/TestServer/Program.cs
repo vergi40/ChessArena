b99df48e438a2b969f2a30a67711cb34aa8ac3fd
Teemu Laine <teemu.laine@etteplanmore.com>
2020-10-10 01:53:18 +0300

    Working rpc connection test in localhost
    * Run TestServer.exe
    * Run vergiBlue.exe
    * Should get playerwhite initialization as response

--- Clients/vergiBlue/TestServer/Program.cs
+++ Clients/vergiBlue/TestServer/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace TestServer
 {
@@ -10,11 +11,34 @@ namespace TestServer
     {
         static void Main(string[] args)
         {
+            const int Port = 30052;
+
+            Server server = new Server
+            {
+                Services = { MovementStream.BindService(new InterfaceImplementation()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("RouteGuide server listening on port " + Port);
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 
     class InterfaceImplementation : MovementStream.MovementStreamBase
     {
+        public override Task<GameStartInformation> Initialize(ClientInformation request, ServerCallContext context)
+        {
+            Console.WriteLine($"Client {request.Name} requested initialize.");
 
+            var response = new GameStartInformation()
+            {
+                WhitePlayer = true
+            };
+            return Task.FromResult(response);
+        }
     }
 }
--- Clients/vergiBlue/vergiBlue/Program.cs
+++ Clients/vergiBlue/vergiBlue/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grpc.Core;
 
 namespace vergiBlue
 {
@@ -10,11 +11,38 @@ namespace vergiBlue
     {
         static void Main(string[] args)
         {
+            Channel channel = new Channel("127.0.0.1:30052", ChannelCredentials.Insecure);
+            var client = new InterfaceImplementation(new MovementStream.MovementStreamClient(channel));
+
+            // YOUR CODE GOES HERE
+            client.Initialize();
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
         }
     }
 
-    class InterfaceImplementation : MovementStream.MovementStreamClient
+    class InterfaceImplementation
     {
+        readonly MovementStream.MovementStreamClient client;
 
+        public InterfaceImplementation(MovementStream.MovementStreamClient client)
+        {
+            this.client = client;
+        }
+
+        public async void Initialize()
+        {
+            var information = new ClientInformation()
+            {
+                Name = "vergiBlue"
+            };
+
+            Console.WriteLine("Initializing client... Getting start information from server.");
+            var startInformation = await client.InitializeAsync(information);
+
+            Console.WriteLine($"Received info: start player: {startInformation.WhitePlayer}.");
+
+        }
     }
 }
