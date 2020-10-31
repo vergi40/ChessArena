11528506ac5bbabb84d07dd3cd36de9b7d073cdc
Teemu Laine <teemu.laine@etteplanmore.com>
2020-10-31 21:12:04 +0200

    Web: Init node.js server project
    * Add packages needed to run gRPC
    * Add test code to call gRPC Initialize

---
+++ Web/nodeserver.js
@@ -0,0 +1,57 @@
+var PROTO_PATH = __dirname + '/../Server/gRPC/protos/GameManager.proto';
+
+var async = require('async');
+var fs = require('fs');
+var parseArgs = require('minimist');
+var path = require('path');
+var _ = require('lodash');
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var manager = grpc.loadPackageDefinition(packageDefinition).GameManager;//namespace
+var client = new manager.GameService('192.168.0.11:50051',
+                                       grpc.credentials.createInsecure());
+
+
+/**
+ * For testing purposes
+ * @param {function} callback 
+ */
+function Initialize(callback) {
+  var next = _.after(1, callback);
+    next();
+  
+  var gameInformation = {
+    name: "node backend client",
+    chess: ""
+  };
+
+  var startInfo = client.Initialize(gameInformation, function(err, feature){
+    if (err) {
+      // process error
+      console.log("Node: Received error" )
+    } else {
+      // process feature
+    }
+  });
+  console.log("Is starting player: " + startInfo.start)
+}
+
+function main() {
+  async.series([
+    Initialize
+  ]);
+}
+
+if (require.main === module) {
+  main();
+}
+
+exports.Initialize = Initialize;
