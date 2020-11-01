dc4915865e954e4d11feefb67afad5d0480ddaed
Jan Lipponen <jan.lipponen@gmail.com>
2020-11-01 02:16:13 +0200

    Classified

--- Server/gRPC/Python/chessarena.py
+++ Server/gRPC/Python/chessarena.py
@@ -1,2 +1,12 @@
-def Initialize():
-    print("Initialize!")
+from enum import Enum
+
+class ChessArena:
+    class Turn(Enum):
+        WHITE = True
+        BLACK = False
+
+    def __init__(self):
+        self._turn = self.Turn.WHITE
+
+    def Act(self):
+        print('Acting..')
