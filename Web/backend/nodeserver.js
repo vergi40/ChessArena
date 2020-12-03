a3ab00778866b440b182df39afeecb45a53ead74
Teemu Laine <teemu.laine@etteplanmore.com>
2020-12-03 12:45:35 +0200

    Web: Implement web service methods

--- Web/backend/nodeserver.js
+++ Web/backend/nodeserver.js
@@ -1,10 +1,10 @@
 var PROTO_PATH = __dirname + '/../../Server/gRPC/protos/GameManager.proto';
 
-var async = require('async');
-var fs = require('fs');
-var parseArgs = require('minimist');
-var path = require('path');
-var _ = require('lodash');
+// var async = require('async');
+// var fs = require('fs');
+// var parseArgs = require('minimist');
+// var path = require('path');
+// var _ = require('lodash');
 var grpc = require('grpc');
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
@@ -15,43 +15,46 @@ var packageDefinition = protoLoader.loadSync(
      defaults: true,
      oneofs: true
     });
-var manager = grpc.loadPackageDefinition(packageDefinition).GameManager;//namespace
-var client = new manager.GameService('192.168.0.11:50051',
-                                       grpc.credentials.createInsecure());
-
+var protoDescription = grpc.loadPackageDefinition(packageDefinition);//namespace
+var manager = protoDescription.GameManager;//namespace
+var stub = new manager.WebService('localhost:50052', grpc.credentials.createInsecure());
 
 /**
  * For testing purposes
  * @param {function} callback 
  */
-function Initialize(callback) {
-  var next = _.after(1, callback);
-    next();
-  
-  var gameInformation = {
-    name: "node backend client",
-    chess: ""
-  };
+function TestInitialize(){
+  // var next = _.after(1, callback);
+  // next();
 
-  var startInfo = client.Initialize(gameInformation, function(err, feature){
+  var request = {message: "ping"};
+  stub.Ping(request, function(err, response) {
     if (err) {
       // process error
-      console.log("Node: Received error" )
-    } else {
+      console.log("Process error: " + err);
+    } 
+    else {
       // process feature
+      if(response == "pong"){
+        console.log("Received ping response from GameManager.")
+      }
+      else{
+        console.log("Received faulty response from GameManager: " + response)
+      }
     }
   });
-  console.log("Is starting player: " + startInfo.start)
 }
 
-function main() {
-  async.series([
-    Initialize
-  ]);
-}
+TestInitialize();
 
-if (require.main === module) {
-  main();
-}
+// function main() {
+//   async.series([
+//     TestInitialize
+//   ]);
+// }
 
-exports.Initialize = Initialize;
+// if (require.main === module) {
+//   main();
+// }
+
+// exports.Initialize = TestInitialize;
