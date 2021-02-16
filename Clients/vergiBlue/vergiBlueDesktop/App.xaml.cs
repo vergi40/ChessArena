e7ac00d07879d104788bbaa1816fb6ed535298d7
Teemu Laine <teemu.laine@etteplanmore.com>
2021-02-16 23:09:42 +0200

    vergiBlue: Launch and link view and viewmodel from code-behind
    * This project setup should be default project start for all WPF projects

--- Clients/vergiBlue/vergiBlueDesktop/App.xaml.cs
+++ Clients/vergiBlue/vergiBlueDesktop/App.xaml.cs
@@ -13,5 +13,20 @@ namespace vergiBlueDesktop
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Startup in code-behind.
+        /// https://www.wpf-tutorial.com/wpf-application/working-with-app-xaml/
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+		private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Link in MVVM manner
+            var viewModel = new MainViewModel();
+            var view = new MainView();
+            view.Title = "vergiBlue desktop environment";
+            view.DataContext = viewModel;
+            view.Show();
+        }
     }
 }
---
+++ Clients/vergiBlue/vergiBlueDesktop/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace vergiBlueDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
