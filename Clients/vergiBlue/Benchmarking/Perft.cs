e79ef19ef93df23edfb2e04a071c6cd4c9031f39
Teemu Laine <teemu.laine@etteplan.com>
2022-02-16 23:02:38 +0200

    vergiBlue: Perft complex position and promotion test

--- Clients/vergiBlue/Benchmarking/Perft.cs
+++ Clients/vergiBlue/Benchmarking/Perft.cs
@@ -23,13 +23,50 @@ namespace Benchmarking
         [TestCase(2, ExpectedResult = 400)]
         [TestCase(3, ExpectedResult = 8902)]
         [TestCase(4, ExpectedResult = 197281)]
-        //[TestCase(5, ExpectedResult = 4865609u)]
+        //[TestCase(5, ExpectedResult = 4865609)]
         public long StartPosition(int depth)
         {
             var board = BoardFactory.CreateDefault();
 
             var result = PerftRec(board, depth, true);
             TestContext.WriteLine($"{nameof(StartPosition)} with depth {depth}: node count {result}");
+            return result;
+        }
+
+        /// <summary>
+        /// Promotion, castling, en passant
+        /// http://www.rocechess.ch/perft.html
+        /// </summary>
+        [Test]
+        [TestCase(1, ExpectedResult = 48)]
+        [TestCase(2, ExpectedResult = 2039)]
+        [TestCase(3, ExpectedResult = 97862)]
+        [TestCase(4, ExpectedResult = 4085603)]
+        //[TestCase(5, ExpectedResult = 193690690)]
+        public long GoodPositions_AndrewWagner(int depth)
+        {
+            var fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+            var board = BoardFactory.CreateFromFen(fen, out var whiteStart);
+
+            var result = PerftRec(board, depth, whiteStart);
+            return result;
+        }
+
+        /// <summary>
+        /// http://www.rocechess.ch/perft.html
+        /// </summary>
+        [Test]
+        [TestCase(1, ExpectedResult = 24)]
+        [TestCase(2, ExpectedResult = 496)]
+        [TestCase(3, ExpectedResult = 9483)]
+        [TestCase(4, ExpectedResult = 182838)]
+        //[TestCase(5, ExpectedResult = 3605103)]
+        public long Promotion_AndrewWagner(int depth)
+        {
+            var fen = "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1";
+            var board = BoardFactory.CreateFromFen(fen, out var whiteStart);
+
+            var result = PerftRec(board, depth, whiteStart);
             return result;
         }
 
