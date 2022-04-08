23602e6ee512f81ebfda8306a751b8e278672809
Teemu Laine <teemu.laine@etteplan.com>
2022-04-08 23:17:51 +0300

    vergiBue: Refactor benchmarks

---
+++ Clients/vergiBlue/vergiBlue.Tests/Benchmarker/Design/ArrayNestingBenchmark.cs
@@ -0,0 +1,63 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using vergiBlue.Pieces;
+
+namespace Benchmarker.Design;
+
+[SimpleJob(RunStrategy.Throughput)]
+public class ArrayNestingBenchmark
+{
+    [Params(10000, 100000)]
+    public int N;
+
+    private readonly Pawn[] _singleArray = new Pawn[128];
+    private readonly ArrayForPieceType[] _nestedArray = new ArrayForPieceType[2];
+
+    class ArrayForPieceType
+    {
+        public Pawn[] SingleArray = new Pawn[64];
+    }
+    public ArrayNestingBenchmark()
+    {
+        var blackPawns = new Pawn[64];
+        var whitePawns = new Pawn[64];
+
+        for (int i = 0; i < 64; i++)
+        {
+            var piece = new Pawn(false, (0, 0));
+            blackPawns[i] = piece;
+            _singleArray[i] = piece;
+        }
+
+        for (int i = 0; i < 64; i++)
+        {
+            var piece = new Pawn(true, (0, 0));
+            whitePawns[i] = piece;
+            _singleArray[i + 64] = piece;
+        }
+
+        _nestedArray[0] = new ArrayForPieceType() { SingleArray = blackPawns };
+        _nestedArray[1] = new ArrayForPieceType() { SingleArray = whitePawns };
+    }
+
+
+    [Benchmark]
+    public void SingleLargeArrayOperations()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            _ = _singleArray[0];
+            _ = _singleArray[64];
+        }
+    }
+
+    [Benchmark]
+    public void NestedArrayOperations()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            _ = _nestedArray[0].SingleArray[0];
+            _ = _nestedArray[1].SingleArray[0];
+        }
+    }
+}
---
+++ Clients/vergiBlue/vergiBlue.Tests/Benchmarker/Design/BoolDictVsArrayBenchmark.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using vergiBlue.Pieces;
+
+namespace Benchmarker.Design;
+
+[SimpleJob(RunStrategy.Throughput)]
+public class BoolDictVsArrayBenchmark
+{
+    [Params(10000, 100000)]
+    public int N;
+
+    private readonly Dictionary<bool, Pawn> _dict;
+    private readonly Pawn[] _array;
+    public BoolDictVsArrayBenchmark()
+    {
+        _dict = new Dictionary<bool, Pawn>()
+        {
+            { true, new Pawn(true, (0, 0)) },
+            { false, new Pawn(false, (0, 0)) },
+        };
+
+        _array = new[]
+        {
+            new Pawn(true, (0, 0)),
+            new Pawn(false, (0, 0))
+        };
+    }
+
+
+    [Benchmark]
+    public void ArrayOperations()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            _ = _array[0];
+            _ = _array[1];
+        }
+    }
+
+    [Benchmark]
+    public void ArrayWithConversionOperations()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            _ = _array[BoolToInt(true)];
+            _ = _array[BoolToInt(false)];
+        }
+    }
+
+    private int BoolToInt(bool index)
+    {
+        // true -> white
+        return index ? 1 : 0;
+    }
+
+    [Benchmark]
+    public void BoolDictOperations()
+    {
+        for (int i = 0; i < N; i++)
+        {
+            _ = _dict[true];
+            _ = _dict[false];
+        }
+    }
+
+    // TODO byte array
+}
