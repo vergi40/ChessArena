aa8af29786353d0ef1ef3c62e5e33cde2a4c1f7c
Teemu Laine <teemu.laine@etteplan.com>
2022-05-04 23:39:13 +0300

    vergiBlue: QoL IMove ToString override

--- Clients/vergiBlue/CommonNetStandard/Common/MoveImplementation.cs
+++ Clients/vergiBlue/CommonNetStandard/Common/MoveImplementation.cs
@@ -1,4 +1,5 @@
-﻿using CommonNetStandard.Interface;
+﻿using System.Text;
+using CommonNetStandard.Interface;
 
 namespace CommonNetStandard.Common
 {
@@ -11,5 +12,48 @@ namespace CommonNetStandard.Common
         public bool Castling { get; set; }
         public PromotionPieceType PromotionResult { get; set; }
 
+        public override string ToString()
+        {
+            var message = new StringBuilder();
+            message.Append($"{StartPosition}{EndPosition}");
+            if (PromotionResult != PromotionPieceType.NoPromotion)
+            {
+                message.Append(ConvertPromotion(PromotionResult));
+            }
+
+            if (Check)
+            {
+                message.Append(" (check)");
+            }
+
+            return message.ToString();
+        }
+
+        public static char ConvertPromotion(PromotionPieceType type)
+        {
+            var c = type switch
+            {
+                PromotionPieceType.Queen => 'q',
+                PromotionPieceType.Rook => 'r',
+                PromotionPieceType.Bishop => 'b',
+                PromotionPieceType.Knight => 'n',
+                _ => ' '
+            };
+            return c;
+        }
+
+        public static PromotionPieceType ConvertPromotion(char type)
+        {
+            type = char.ToLower(type);
+            var promotion = type switch
+            {
+                'q' => PromotionPieceType.Queen,
+                'r' => PromotionPieceType.Rook,
+                'b' => PromotionPieceType.Bishop,
+                'n' => PromotionPieceType.Knight,
+                _ => PromotionPieceType.NoPromotion
+            };
+            return promotion;
+        }
     }
 }
