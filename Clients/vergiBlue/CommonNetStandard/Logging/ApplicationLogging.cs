22b55dc7f93f7bfd6556d4d4a6360322f0d7ba8a
Teemu Laine <teemu.laine@etteplan.com>
2022-06-26 14:04:01 +0300

    vergiBlue: Add common logging api that uses MEL api

---
+++ Clients/vergiBlue/CommonNetStandard/Logging/ApplicationLogging.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace CommonNetStandard.Logging
+{
+    /// <summary>
+    /// Use Microsoft.Extensions.Logging API for logging messages.
+    /// Logging provider and configuration can be modified here.
+    /// https://stackify.com/net-core-loggerfactory-use-correctly/
+    /// </summary>
+    public class ApplicationLogging
+    {
+        private static ILoggerFactory? _loggerFactory;
+
+        private static void ConfigureLogger(ILoggerFactory loggerFactory)
+        {
+            // TODO serilog
+
+        }
+
+        public static ILoggerFactory LoggerFactory
+        {
+            get
+            {
+                if (_loggerFactory == null)
+                {
+                    var serilogLogger = new LoggerConfiguration().MinimumLevel.Debug()
+                        .WriteTo.Console()
+                        .WriteTo.File("Logs/test.log")
+                        .CreateLogger();
+
+                    _loggerFactory = new LoggerFactory().AddSerilog(serilogLogger);
+                    ConfigureLogger(_loggerFactory);
+                }
+
+                return _loggerFactory;
+            }
+            set
+            {
+                _loggerFactory = value;
+            }
+        }
+
+        public static ILogger CreateLogger<T>()
+        {
+            return LoggerFactory.CreateLogger<T>();
+        }
+    }
+}
