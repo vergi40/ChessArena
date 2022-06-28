45c80c939847d221a168406772ca588d9e89fd18
Teemu Laine <teemu.laine@etteplan.com>
2022-06-28 23:39:11 +0300

    vergiBlue: Add more information to log format

--- Clients/vergiBlue/CommonNetStandard/Logging/ApplicationLogging.cs
+++ Clients/vergiBlue/CommonNetStandard/Logging/ApplicationLogging.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
+﻿using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
@@ -11,19 +7,21 @@ namespace CommonNetStandard.Logging
 {
     /// <summary>
     /// Use Microsoft.Extensions.Logging API for logging messages.
+    /// MEL API offers support for variety of providers - easy to change later.
     /// Logging provider and configuration can be modified here.
     /// https://stackify.com/net-core-loggerfactory-use-correctly/
+    /// https://stackoverflow.com/questions/46483019/logging-from-static-members-with-microsoft-extensions-logging
     /// </summary>
     public class ApplicationLogging
     {
+        // Notes: Could figure out, how to write short events in console, but full events in file
+        // Or to have own dedicated log files for each
         private static ILoggerFactory? _loggerFactory;
 
         private static void ConfigureLogger(ILoggerFactory loggerFactory)
         {
-            // TODO serilog
-            // TODO if namespace is vergiBlueConsole, write to console infolevel and upwards
-            // Debug should be written to file
-
+            // TODO not sure yet should there be separate configuring for console and desktop project
+            // Or have dedicated logger for each
         }
 
         public static ILoggerFactory LoggerFactory
@@ -32,9 +30,14 @@ namespace CommonNetStandard.Logging
             {
                 if (_loggerFactory == null)
                 {
+                    // %date [%thread] %level %logger{1} | %message%newline"
                     var serilogLogger = new LoggerConfiguration().MinimumLevel.Debug()
-                        .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
-                        .WriteTo.File("Logs/test.log")
+                        .WriteTo.Console(
+                            restrictedToMinimumLevel: LogEventLevel.Information, 
+                            outputTemplate: "{Message}{NewLine}")
+                        .WriteTo.File("Logs/vergiBlue.log", 
+                            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} {Class} | {Method} {Message:lj}{NewLine}{Exception}")
+                        .Enrich.WithThreadId()
                         .CreateLogger();
 
                     _loggerFactory = new LoggerFactory().AddSerilog(serilogLogger);
