d4aa5fac3e73bf16b0c10a4381e543f0e0e69265
Teemu Laine <teemu.laine@etteplan.com>
2022-06-30 08:11:18 +0300

    vergiBlue: Add Serilog with ExpressionTemplate

--- Clients/vergiBlue/CommonNetStandard/Logging/ApplicationLogging.cs
+++ Clients/vergiBlue/CommonNetStandard/Logging/ApplicationLogging.cs
@@ -4,6 +4,7 @@ using NLog.Config;
 using NLog.Extensions.Logging;
 using Serilog;
 using Serilog.Events;
+using Serilog.Templates;
 using ILogger = Microsoft.Extensions.Logging.ILogger;
 using LogLevel = NLog.LogLevel;
 
@@ -21,6 +22,8 @@ namespace CommonNetStandard.Logging
         // Notes: Could figure out, how to write short events in console, but full events in file
         // Or to have own dedicated log files for each
         private static ILoggerFactory? _loggerFactory;
+
+        private const string _fileName = "Logs/vergiBlue.log";
         
         public static ILoggerFactory LoggerFactory
         {
@@ -28,7 +31,7 @@ namespace CommonNetStandard.Logging
             {
                 if (_loggerFactory == null)
                 {
-                    _loggerFactory = CreateNLogLoggerFactory();
+                    _loggerFactory = CreateSerilogLoggerFactory();
                 }
 
                 return _loggerFactory;
@@ -46,14 +49,20 @@ namespace CommonNetStandard.Logging
 
         private static ILoggerFactory CreateSerilogLoggerFactory()
         {
-            // Example output. Notice the class name will be full name
-            // 2022-06-29 22:47:56,405 [1] INF vergiBlueDesktop.Views.MainViewModel | History: 1: e2 to e4
+            // Example output. Use ExpressionTemplate to parse only class name
+            // 2022-06-30 08:09:38,584 [1] INF MainViewModel | History: 3: d1 to b3
+            var template = new ExpressionTemplate("{@t:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {@l:u3} " +
+                                                    "{Substring(SourceContext, LastIndexOf(SourceContext, '.') + 1)} " +
+                                                    "| {@m}\n{@x}");
+
+            var originalTemplate =
+                "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} {SourceContext} | {Message:lj}{NewLine}{Exception}";
+            
             var serilogLogger = new LoggerConfiguration().MinimumLevel.Debug()
                 .WriteTo.Console(
                     restrictedToMinimumLevel: LogEventLevel.Information,
                     outputTemplate: "{Message}{NewLine}")
-                .WriteTo.File("Logs/vergiBlue.log",
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss,fff} [{ThreadId}] {Level:u3} {SourceContext} | {Message:lj}{NewLine}{Exception}")
+                .WriteTo.File(template, _fileName)
                 .Enrich.WithThreadId()
                 .CreateLogger();
 
@@ -75,7 +84,7 @@ namespace CommonNetStandard.Logging
                          "${message}${exception}";
 
             // Targets where to log to: File and Console
-            var logFileTarget = new NLog.Targets.FileTarget("logfile") { FileName = "Logs/vergiBlue.log", Layout = layout};
+            var logFileTarget = new NLog.Targets.FileTarget("logfile") { FileName = _fileName, Layout = layout};
             var consoleTarget = new NLog.Targets.ConsoleTarget("logconsole"){Layout = "${message"};
 
             var config = new LoggingConfiguration();
